27c2aa6968f33f8520714ceb7f37f4c9301b9010
Spongebob Longpants <spongeboblongpants9@gmail.com>
2022-12-11 15:47:57 -0800

    I love you

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * Program for using printf to output stuff
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a pice of art, \n");
+	return (0);
+	
+}
